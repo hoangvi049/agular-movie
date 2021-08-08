0239db251f05ca3ff7cd0c28a7aef7aff1cda2cd
hoangvi <hoangvi0409@gmail.com>
2021-08-08 17:03:58 +0700

    1

--- app.module.ts
+++ app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
+import { DetailModule } from './detail/detail.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HomeModule, RoutingModule, RouterModule
+    BrowserModule, CoreModule, RoutingModule, RouterModule, BrowserAnimationsModule,
+    HomeModule, DetailModule,AuthenticationModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- home/home.module.ts
+++ home/home.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeIndexComponent } from './home-index/home-index.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MovieComponent } from './movie/movie.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
-    HomeIndexComponent
+    HomeIndexComponent,
+    MovieComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, MatButtonModule, BrowserAnimationsModule, FlexLayoutModule, MatCardModule
   ],
   exports: [HomeIndexComponent]
 })
+
+
 export class HomeModule { }
